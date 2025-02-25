da494b84a55b4042b2ea4ef69fca3d92ff4e63ce
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-25 16:36:37 +0530

    [Saurabh] Add Logging Levels && ENVIRONMENT

--- src/main/java/com/employee/payroll2/Payroll2Application.java
+++ src/main/java/com/employee/payroll2/Payroll2Application.java
@@ -1,13 +1,17 @@
 package com.employee.payroll2;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class Payroll2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Payroll2Application.class, args);
+		ApplicationContext context = SpringApplication.run(Payroll2Application.class, args);
+		log.info("EMployee Payroll App Started in Environment",context.getEnvironment().getProperty("enviroment"));
 	}
 
 }
