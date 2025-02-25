217e292914b0eddf4a4d71a4b78de0a55c9b8d7f
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-25 15:21:16 +0530

    [Saurabh] Add Lombok Library to autogenerate getters and setters

--- src/main/java/com/employee/payroll2/model/EmployeeEntity.java
+++ src/main/java/com/employee/payroll2/model/EmployeeEntity.java
@@ -1,30 +1,19 @@
 package com.employee.payroll2.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "employees")
 public class EmployeeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private Double salary;
-
-    public EmployeeEntity() {}
-
-    public EmployeeEntity(Long id ,String name, Double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public Double getSalary() { return salary; }
-    public void setSalary(Double salary) { this.salary = salary; }
 }
