4e6d19026f14bb30189f19131481090dfd84578f
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-27 11:57:27 +0530

    [Saurabh] Add User Friendly Error Response

---
+++ src/main/java/com/employee/payroll2/exception/ResponseEntityExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.employee.payroll2.exception;
+
+import com.employee.payroll2.dto.EmployeeDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ResponseEntityExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<Map<String,String>> exceptionHandler(MethodArgumentNotValidException e){
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().
+                forEach(error ->
+                errors.put(error.getField(),
+                        error.getDefaultMessage()));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
