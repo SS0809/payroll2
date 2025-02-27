27a67d033f555ee476c40370deedb14f796113bc
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-27 15:11:54 +0530

    [Saurabh] Add remaining properties to the Payroll DTO and Model

--- src/main/java/com/employee/payroll2/dto/EmployeeDTO.java
+++ src/main/java/com/employee/payroll2/dto/EmployeeDTO.java
@@ -4,6 +4,9 @@ package com.employee.payroll2.dto;
 import jakarta.validation.constraints.*;
 import lombok.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,4 +18,9 @@ public class EmployeeDTO {
     @DecimalMin(value = "0.0", message = "salary is in min range")
     @DecimalMax(value = "9999999.0", message = "salary is in max range")
     private Double salary;
+    private String gender;
+    private LocalDate startdate;
+    private String note;
+    private String profilePic;
+    private List<String> departments;
 }
--- src/main/java/com/employee/payroll2/model/EmployeeEntity.java
+++ src/main/java/com/employee/payroll2/model/EmployeeEntity.java
@@ -1,9 +1,13 @@
 package com.employee.payroll2.model;
 
 import jakarta.persistence.*;
+import jdk.dynalink.linker.LinkerServices;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Data
@@ -16,4 +20,9 @@ public class EmployeeEntity {
     private Long id;
     private String name;
     private Double salary;
+    private String gender;
+    private LocalDate startdate;
+    private String note;
+    private String profilePic;
+    private List<String> departments;
 }
